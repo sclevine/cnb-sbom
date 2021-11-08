2fbb6365fa8bda42dbe022181cdbafc3cda15bf3
Stephen Levine <stephen.levine@gmail.com>
2021-11-07 23:34:48 -0500

    get/attach both work
    Signed-off-by: Stephen Levine <stephen.levine@gmail.com>

--- go.mod
+++ go.mod
@@ -5,10 +5,13 @@ go 1.17
 require github.com/google/go-containerregistry v0.6.0
 
 require (
+	github.com/containerd/stargz-snapshotter/estargz v0.7.0 // indirect
 	github.com/docker/cli v20.10.7+incompatible // indirect
 	github.com/docker/distribution v2.7.1+incompatible // indirect
 	github.com/docker/docker v20.10.7+incompatible // indirect
 	github.com/docker/docker-credential-helpers v0.6.3 // indirect
+	github.com/golang/snappy v0.0.3 // indirect
+	github.com/klauspost/compress v1.13.0 // indirect
 	github.com/opencontainers/go-digest v1.0.0 // indirect
 	github.com/opencontainers/image-spec v1.0.1 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
--- main.go
+++ main.go
@@ -4,16 +4,20 @@ import (
 	"archive/tar"
 	"encoding/json"
 	"errors"
+	"fmt"
 	"io"
 	"log"
 	"os"
+	"path"
 	"path/filepath"
 	"strings"
 
 	"github.com/google/go-containerregistry/pkg/authn"
 	gname "github.com/google/go-containerregistry/pkg/name"
 	v1 "github.com/google/go-containerregistry/pkg/v1"
+	"github.com/google/go-containerregistry/pkg/v1/mutate"
 	"github.com/google/go-containerregistry/pkg/v1/remote"
+	"github.com/google/go-containerregistry/pkg/v1/tarball"
 )
 
 const (
@@ -23,12 +27,116 @@ const (
 )
 
 func main() {
-	if err := run(os.Args[1], "."); err != nil {
-		log.Fatalf("Error: %s", err)
+	if len(os.Args) < 2 || os.Args[1] == "-h" {
+		log.Fatalf("Usage: %s [ attach | get ]", os.Args[0])
 	}
+	switch os.Args[1] {
+	case "attach":
+		if len(os.Args) != 5 {
+			log.Fatalf("Usage: %s attach [image] [path] [ext]", os.Args[0])
+		}
+		if err := addBaseSBoM(os.Args[2], os.Args[3], os.Args[4]); err != nil {
+			log.Fatalf("Error: %s", err)
+		}
+	case "get":
+		if len(os.Args) != 3 {
+			log.Fatalf("Usage: %s get [image]", os.Args[0])
+		}
+		if err := getSBoM(os.Args[2], "."); err != nil {
+			log.Fatalf("Error: %s", err)
+		}
+	default:
+		log.Fatalf("Error: invalid command `%s'", os.Args[1])
+	}
+
 }
 
-func run(image, dir string) error {
+func addBaseSBoM(image, filepath, ext string) error {
+	ref, err := gname.ParseReference(image, gname.WeakValidation)
+	if err != nil {
+		return err
+	}
+	img, err := remote.Image(ref, remote.WithAuthFromKeychain(authn.DefaultKeychain))
+	if err != nil {
+		return err
+	}
+	digest, err := img.Digest()
+	if err != nil {
+		return err
+	}
+	dstPath := path.Join("/cnb/sbom", digest.Hex[:8]+"."+strings.TrimPrefix(ext, "."))
+	layer, err := createLayer(filepath, dstPath)
+	if err != nil {
+		return err
+	}
+	img, err = mutate.AppendLayers(img, layer)
+	if err != nil {
+		return err
+	}
+	diffID, err := layer.DiffID()
+	if err != nil {
+		return err
+	}
+	cf, err := img.ConfigFile()
+	if err != nil {
+		return err
+	}
+	if cf.Config.Labels == nil {
+		cf.Config.Labels = make(map[string]string, 1)
+	}
+	cf.Config.Labels[baseLabel] = diffID.String()
+	img, err = mutate.ConfigFile(img, cf)
+	if err != nil {
+		return err
+	}
+	return remote.Write(ref, img, remote.WithAuthFromKeychain(authn.DefaultKeychain))
+}
+
+func createLayer(fromPath, toPath string) (v1.Layer, error) {
+	return tarball.LayerFromOpener(func() (io.ReadCloser, error) {
+		f, err := os.Open(fromPath)
+		if err != nil {
+			return nil, err
+		}
+		fd, err := f.Stat()
+		if err != nil {
+			return nil, err
+		}
+		return tarFile(f, toPath, fd.Size()), nil
+	})
+}
+
+func tarFile(r io.ReadCloser, name string, size int64) io.ReadCloser {
+	out, w := io.Pipe()
+	go func() {
+		defer r.Close()
+		defer w.Close() // always nil + never overrides
+		tw := tar.NewWriter(w)
+
+		// are docker layers ustar or gnu tar?
+		header := &tar.Header{
+			Name: name,
+			Size: size,
+			Mode: 0644,
+		}
+
+		if err := tw.WriteHeader(header); err != nil {
+			w.CloseWithError(err)
+			return
+		}
+		if _, err := io.Copy(tw, r); err != nil {
+			w.CloseWithError(err)
+			return
+		}
+		if err := tw.Close(); err != nil {
+			w.CloseWithError(err)
+			return
+		}
+	}()
+	return out
+}
+
+func getSBoM(image, dir string) error {
 	ref, err := gname.ParseReference(image, gname.WeakValidation)
 	if err != nil {
 		return err
@@ -41,11 +149,11 @@ func run(image, dir string) error {
 	if err != nil {
 		return err
 	}
-	var appDigest, baseDigest string
+	var appDiffID, baseDiffID string
 	if cf.Config.Labels != nil {
-		appDigest = cf.Config.Labels[appLabel]
-		baseDigest = cf.Config.Labels[baseLabel]
-		if appDigest == "" {
+		appDiffID = cf.Config.Labels[appLabel]
+		baseDiffID = cf.Config.Labels[baseLabel]
+		if appDiffID == "" {
 			var md struct {
 				BOM struct {
 					SHA string
@@ -54,24 +162,24 @@ func run(image, dir string) error {
 			if err := json.Unmarshal([]byte(cf.Config.Labels[metadataLabel]), &md); err != nil {
 				return err
 			}
-			appDigest = md.BOM.SHA
+			appDiffID = md.BOM.SHA
 		}
 	}
-	if err := writeLayer(img, appDigest, dir, "app", "/layers/sbom"); err != nil {
-		return err
+	if err := extractLayer(img, baseDiffID, dir, "/cnb/sbom", "base"); err != nil {
+		return fmt.Errorf("could not retrieve base image SBoM: %w", err)
 	}
-	if err := writeLayer(img, baseDigest, dir, "base", "/cnb/sbom"); err != nil {
-		return err
+	if err := extractLayer(img, appDiffID, dir, "/layers/sbom", "app"); err != nil {
+		return fmt.Errorf("could not retrieve app SBoM: %w", err)
 	}
 	return nil
 }
 
-func writeLayer(img v1.Image, digest, dir, prefix, strip string) error {
-	hash, err := v1.NewHash(digest)
+func extractLayer(img v1.Image, diffID, toDir, fromDir, prefix string) error {
+	hash, err := v1.NewHash(diffID)
 	if err != nil {
 		return err
 	}
-	appLayer, err := img.LayerByDigest(hash)
+	appLayer, err := img.LayerByDiffID(hash)
 	if err != nil {
 		return err
 	}
@@ -80,10 +188,10 @@ func writeLayer(img v1.Image, digest, dir, prefix, strip string) error {
 		return err
 	}
 	defer tar.Close()
-	return untarSBOMs(tar, dir, prefix, strip)
+	return untarSBOMs(tar, toDir, fromDir, prefix)
 }
 
-func untarSBOMs(r io.Reader, dir, prefix, strip string) error {
+func untarSBOMs(r io.Reader, toDir, fromDir, prefix string) error {
 	tr := tar.NewReader(r)
 	for {
 		header, err := tr.Next()
@@ -93,12 +201,12 @@ func untarSBOMs(r io.Reader, dir, prefix, strip string) error {
 		if err != nil {
 			return err
 		}
-		if !strings.HasPrefix(header.Name, strip) {
+		if !strings.HasPrefix(header.Name, fromDir) {
 			continue
 		}
-		s := strings.TrimPrefix(strings.TrimPrefix(header.Name, strip), "/")
+		s := strings.TrimPrefix(strings.TrimPrefix(header.Name, fromDir), "/")
 		name := prefix + "." + strings.Join(strings.Split(s, "/"), ".")
-		size, err := writeFile(filepath.Join(dir, name), tr)
+		size, err := writeFile(filepath.Join(toDir, name), tr)
 		if err != nil {
 			return err
 		}
